b825991993f214d6ff20f6f1da0934b42d0aede5
yuki <yuki39357@gmail.com>
2026-01-06 00:51:08 +0900

    setFaviconUrl

--- code.js
+++ code.js
@@ -3,6 +3,7 @@ function doGet() {
   const htmlOutput = HtmlService.createTemplateFromFile("index").evaluate();
   htmlOutput
   .setTitle('ドッカンバトルステータス計算')
+  .setFaviconUrl("https://yuki39357-cmyk.github.io/gas-status-calculator/favicon.png")
   .addMetaTag('viewport','width=device-width,initial-scale=1.0,maximum-scale=1.0');
   return htmlOutput;
 }
